8ddd02c6c96ab4e41ba6491dfca07164902725f6
reinaldo110 <>
2022-04-09 04:57:29 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -112,18 +112,21 @@ Pair * searchMap(HashMap * map,  char * key) {
 }
 
 Pair * firstMap(HashMap * map) {
-    map->current = 0;
-    int pos = map->current;
-    if (map->buckets[pos]->key == NULL)
+    int i = 0;
+    if (strcmp(map->buckets[i]->key, NULL) == 0)
     {
-        while (map->buckets[pos]->key == NULL)
+        while (strcmp(map->buckets[i]->key, NULL) == 0)
         {
-            map->current++;
-            pos++;
+            i++;
         }
-        return map->buckets[pos];
+        map->current = i;
+        return map->buckets[i];
     }
-    else return map->buckets[0];
+    else
+    {
+        map->current = i;
+        return map->buckets[i];
+    }
 }
 
 Pair * nextMap(HashMap * map) {
