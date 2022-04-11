0f38a2d92fff94722497d19b629bad78ec07c27c
reinaldo110 <>
2022-04-11 03:37:59 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -58,6 +58,7 @@ void insertMap(HashMap * map, char * key, void * value) {
 
 void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
+    int mapCap;
     int i = 0;
     Pair ** aux = (Pair **) calloc (map->capacity, sizeof(Pair **));
     aux = map->buckets;
