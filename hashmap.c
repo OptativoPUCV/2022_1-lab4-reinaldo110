8bc5c878a4bacd4d4f7628e8dc44e191d995c139
reinaldo110 <>
2022-04-11 03:42:11 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -60,7 +60,7 @@ void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
     int i = 0;
     Pair ** aux;
-    aux = *map->buckets;
+    aux = map->buckets;
     map->capacity *= 2;
     map->buckets = (Pair **) calloc (map->capacity, sizeof(Pair **));
     while (i < map->capacity)
